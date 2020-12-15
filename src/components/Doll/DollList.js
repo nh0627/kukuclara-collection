07ecc69324114cc49a25ba6641ed24598a854264
Nahyeon Lee <naboee0627@gmail.com>
2020-12-15 22:26:52 +0200

    Attach more pros to DollList

--- src/components/Doll/DollList.js
+++ src/components/Doll/DollList.js
@@ -5,16 +5,23 @@ import { selectDoll } from '../../actions';
 class DollList extends Component {
     renderList() {
         return this.props.dolls.map(doll => {
+
+            const specialCorner = <div class="ui yellow ribbon label">Special</div>;
+
             return (
                 <div className="card" key={doll.kcId}>
-                    <div className="image">
-                        <img src={doll.img} alt="kukuclara"/>
+                    <div className="ui fluid image">
+                        {(doll.specialYn === "Y") ? specialCorner : ""}
+                        <img src={doll.img} alt="kukuclara" />
                     </div>
                     <div className="content">
                         <div className="header">{doll.name} {doll.type}</div>
                         <div className="meta">
                             <div className="group">{doll.series}</div>
                         </div>
+                    </div>
+                    <div class="extra content">
+                        <span class="right floated">{doll.date}</span>
                     </div>
                 </div>
             );
@@ -37,4 +44,4 @@ const mapStateToProps = state => {
 }
 
 
-export default connect(mapStateToProps, { selectDoll})(DollList);
+export default connect(mapStateToProps, { selectDoll })(DollList);
