7c125b67abd4667290a0fb5a423b164cc2d94e70
Nahyeon Lee <naboee0627@gmail.com>
2020-12-17 04:28:22 +0200

    change searchbox component name

--- src/components/UI/Menu.js
+++ src/components/UI/Menu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SearchDropdown from './SearchDropdown'
+import SearchBox from './SearchBox'
 
 const Menu = () => {
     return (
@@ -12,7 +12,7 @@ const Menu = () => {
             <div className="right menu">
                     <div className="ui item icon top right pointing dropdown button">
                         <i className="search icon"></i>
-                        <SearchDropdown />
+                        <SearchBox />
                     </div>
                 </div>
             </div>
---
+++ src/components/UI/SearchBox.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+
+let SearchBox = (props) => {
+
+    const { handleSubmit, types } = props;
+
+    const tagColors = ["yellow", "olive", "teal", "blue"];
+
+    const typeTags = (type, i) => {
+        const { code, name } = type;
+
+        return (
+            <div className="item" key={code}>
+                <div className={`ui empty circular label ${tagColors[i]}`}></div>
+                { name}
+            </div>
+        );
+    };
+
+    const onSubmit = (val) => console.log(1, val);
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="menu">
+                <div className="ui search icon input">
+                    <i className="search icon"></i>
+                    <Field component="input" type="text" name="search" placeholder="Search item..." />
+                </div>
+                <div className="divider"></div>
+                <div className="header">
+                    <i className="tags icon"></i>
+                Filter by type
+            </div>
+                {types.map((type, i) => typeTags(type, i))}
+            </div>
+        </form>
+    );
+};
+
+const mapStateToProps = state => {
+    return { types: state.types };
+}
+
+// Decorate with reduxForm(). It will read the initialValues prop provided by connect()
+SearchBox = reduxForm({form: 'initializeFromState'})(SearchBox)
+
+SearchBox = connect(mapStateToProps)(SearchBox)
+
+export default SearchBox;
+
+
