0518a62867e36962cf219c8afd67f5ec5c2f1a2e
Nahyeon Lee <naboee0627@gmail.com>
2020-12-18 01:47:59 +0200

    정리정리

--- src/components/Doll/DollCard.js
+++ src/components/Doll/DollCard.js
@@ -3,6 +3,7 @@ import React from 'react';
 const DollCard = prop => {
 
     const { doll } = prop;
+    const { specialYn, img, name, type, series, date } = doll;
 
     const putSpecialRibbon = (specialYn) => {
         const specialCorner = <div className="ui yellow ribbon label">Special</div>;
@@ -12,18 +13,18 @@ const DollCard = prop => {
     return (
         <div className="card">
             <div className="ui fluid image">
-                { putSpecialRibbon(doll.specialYn) }
-                <img src={doll.img} alt="kukuclara" />
+                {putSpecialRibbon(specialYn)}
+                <img src={img} alt="kukuclara" />
             </div>
             <div className="content">
-                <div className="header">{doll.name} {doll.type}</div>
+                <div className="header">{name} {type}</div>
                 <div className="meta">
-                    <div className="group">{doll.series}</div>
+                    <div className="group">{series}</div>
                 </div>
             </div>
             <div className="extra content">
                 <i className="heart outline icon"></i>
-                <span className="right floated">{doll.date}</span>
+                <span className="right floated">{date}</span>
             </div>
         </div>
     );
