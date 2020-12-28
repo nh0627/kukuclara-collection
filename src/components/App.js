1fd0b53c23dcc51f7e60f9da7c6576620da4c642
Nahyeon Lee <naboee0627@gmail.com>
2020-12-28 20:55:45 +0200

    Add filter modal component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import DollList from './Doll/DollList';
-import MenuBar from './UI/MenuBar';
+import MenuBar from './UI/Menu/MenuBar';
 import { Container } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 
---
+++ src/components/UI/Menu/MenuBar.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import FilterDropDown from './FilterDropdown';
+import FilterModal from '../Modal/FilterModal';
+import { Menu, Container, Icon } from 'semantic-ui-react';
+
+export default class MenuBar extends Component {
+    state = { activeItem: '' };
+
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+    render() {
+        const { activeItem } = this.state;
+
+        return (
+            <Menu fixed='top' borderless>
+                <Container>
+                    <Menu.Item
+                        link
+                        name='sidemenu'
+                        active={activeItem === 'sidemenu'}
+                        onClick={this.handleItemClick}
+                    >
+                        <Icon name='content' />
+                    </Menu.Item>
+                    <Menu.Item header>Kuku Clara</Menu.Item>
+                    <Menu.Menu position='right'>
+                        <FilterModal trigger={
+                            <Menu.Item
+                                link
+                                name='filter'
+                                active={activeItem === 'filter'}
+                                onClick={this.handleItemClick}
+                            >
+                                <Icon name='filter' />
+                            </Menu.Item>} />
+                        <FilterDropDown />
+                    </Menu.Menu>
+                </Container>
+            </Menu>
+
+
+        );
+    };
+};
