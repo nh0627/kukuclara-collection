52265ec917eb054d619332c456a17f5957ef176c
Nahyeon Lee <naboee0627@gmail.com>
2021-01-09 22:20:08 +0200

    키워드 작동

--- src/reducers/dollsReducer.js
+++ src/reducers/dollsReducer.js
@@ -17,6 +17,12 @@ const parseObjWithKeys = (dolls) => {
     return dollObj;
 }
 
+const searchDollWithTerm = (doll, term) => {
+    const { type, series, name } = doll;
+    const fullname = `${series} ${name} ${type}`;
+    return fullname.toLowerCase().indexOf(term.toLowerCase()) > -1;
+};
+
 export default (state = [], action) => {
     switch (action.type) {
         case FETCH_DOLLS:
@@ -25,34 +31,46 @@ export default (state = [], action) => {
         case SEARCH_DOLLS:
             const { term } = action.payload;
             if (typeof term === "undefined") return parseObjWithKeys(loadedData);
-            const foundDolls = loadedData.filter(doll => {
-                const { type, series, name } = doll;
-                const fullname = `${series} ${name} ${type}`;
-                return fullname.toLowerCase().indexOf(term.toLowerCase()) > -1;
-            });
+            const foundDolls = loadedData.filter(doll => searchDollWithTerm(doll, term));
             return { ...parseObjWithKeys(foundDolls) };
         case FILTER_DOLLS:
             const submitData = action.payload;
-            // 체크박스그룹명 리스트를 store(다른 reducer)에서 가져올 수 없어서 submit 시 컴포넌트에서 전송해서 사용함
-            const filterKeys = submitData[CHECKBOX_GROUPS];
+            const filterKeys = submitData[CHECKBOX_GROUPS]; // Get the name(label) of checkbox (group) fields from submit data
+            const selectedFilterGroups = []; // data from checkboxes
+            const selectcedFilters = []; // data from other fields
 
-            const selectedFilterKeys = Object.keys(submitData).filter(e => filterKeys.indexOf(e) > -1);
+            for (const key in submitData) {
+                // Get data from checkbox groups
+                if (filterKeys.indexOf(key) > -1) {
+                    selectedFilterGroups.push(key);
+                    // data from not checkboxe fields, but without the "checkbox group names"
+                } else if (key !== CHECKBOX_GROUPS) {
+                    selectcedFilters.push(key);
+                }
+            }
+
             // TODO: Make it less complecated(if possible😭)
             // Retrieve the loaded doll data to filter it
             const filteredDolls = loadedData.filter(doll => {
 
-                const matchedFilterKeys = selectedFilterKeys.filter(keyName => {
-                        // Change/Match to the loaded object's(dolls) property name(e.g. types => typeCode)
-                        const parsedKeyName = `${(Pluralize.isSingular(keyName)) ? keyName : Pluralize.singular(keyName)}Code`;
-                        const codeFromLoadedData = doll[parsedKeyName];
-                        const codesFromSubmitData = submitData[keyName];
+                // Data from checkbox groups
+                const matchedKeysFromFilterGroups = selectedFilterGroups.filter(keyName => {
+                    // Change/Match to the loaded object's(dolls) property name(e.g. types => typeCode)
+                    const parsedKeyName = `${(Pluralize.isSingular(keyName)) ? keyName : Pluralize.singular(keyName)}Code`;
+                    const codeFromLoadedData = doll[parsedKeyName];
+                    const codesFromSubmitData = submitData[keyName];
                     // Check if one is matching at least
                     // In other words, within one category(e.g. skin, hair color) it is OR(union)
                     return codesFromSubmitData.indexOf(codeFromLoadedData) > -1;
                 });
 
+                const matchedKeysFromFilters = selectcedFilters.filter(keyName => {
+                    if (keyName === 'term') return !searchDollWithTerm(doll, submitData[keyName]);
+                });
+
+                const matchedKeys = [...matchedKeysFromFilterGroups, ...matchedKeysFromFilters];
                 // It is AND(intersection) between different categories
-                return matchedFilterKeys.sort().join(",") === selectedFilterKeys.sort().join(",");
+                return matchedKeys.sort().join(",") === selectedFilterGroups.sort().join(",");
             });
 
             return { ...parseObjWithKeys(filteredDolls) };
