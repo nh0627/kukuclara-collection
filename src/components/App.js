658dfdddf0c0500336278f19cf16586812ca0b4f
Nahyeon Lee <naboee0627@gmail.com>
2021-01-17 23:41:48 +0200

    feat: Add loader to doll list

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
-import React from "react";
-import DollList from "./Doll/DollList";
+import React, { Suspense, lazy } from "react";
 import MenuBar from "./UI/MenuBar";
 import { Container } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
+import Loader from "./UI/Loader";
 
 const App = () => {
-  const mainListStyle = {"marginTop": "4.5rem"};
+  const mainListStyle = { "marginTop": "4.5rem" };
+  const DollList = lazy(() => import("./Doll/DollList"));
+
   return (
     <div>
       <MenuBar />
       <Container style={mainListStyle}>
-        <DollList />
+        <Suspense fallback={<Loader />}>
+          <DollList />
+        </Suspense>
       </Container>
     </div>
   );
