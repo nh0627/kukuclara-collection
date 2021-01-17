5d56953e0d809fdcab2d2d9f8177218981b2096b
Nahyeon Lee <naboee0627@gmail.com>
2021-01-17 21:16:39 +0200

    fix: Fix checkbox component

--- src/components/UI/Field.js
+++ src/components/UI/Field.js
@@ -34,7 +34,7 @@ export const CheckboxField = ({
     <Form.Checkbox
         {...input}
         {...rest}
-        checked={value}
+        checked={value===true}
         onChange={(param, { checked }) => onChange(checked)}
         type="checkbox"
     />
