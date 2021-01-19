a55c8c7bb1bc17582f8e17eadb3d7acace31bbe5
Nahyeon Lee <naboee0627@gmail.com>
2021-01-20 00:15:38 +0200

    feat: Make dropdown search work on change, not submit

--- src/components/Filter/FilterDropdown.js
+++ src/components/Filter/FilterDropdown.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Field, reduxForm } from "redux-form";
 import { filterDolls } from "../../actions";
-import { Dropdown, Icon } from "semantic-ui-react";
+import { Dropdown, Icon, Input } from "semantic-ui-react";
 
 let FilterDropDown = (props) => {
-    const { types, handleSubmit } = props;
+    const { types } = props;
     const [term, setTerm] = React.useState("");
     const [activeTag, setActiveTag] = React.useState("");
 
-    const onSubmit = () => {
+    const filterByTerm = (term) => {
+        setTerm(term);
         const filterGroups = (activeTag === "") ? [] : ["types"];
         const returnObj = { term, types: [activeTag], filterGroups };
         props.filterDolls(returnObj);
     }
 
-    const filterList = (code) => {
+    const filterByTag = (code) => {
         (activeTag !== code) ? setActiveTag(code) : setActiveTag("");
         const searchTag = (code === activeTag) ? "" : code;
         const filterGroups = (searchTag === "") ? [] : ["types"];
@@ -31,33 +31,29 @@ let FilterDropDown = (props) => {
                 {...options}
                 active={activeTag === code}
                 onClick={() => {
-                    filterList(code);
+                    filterByTag(code);
                 }} />
         );
     };
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <Dropdown item simple icon="search">
-                <Dropdown.Menu>
-                    <div className="ui input icon" >
-                        <Field component="input" type="text" name="term" onChange={e => setTerm(e.target.value)} placeholder="Search..." />
-                        <Icon aria-hidden="true" name="search" />
-                    </div>
-                    <Dropdown.Divider />
-                    <Dropdown.Header>FILTER BY TYPE</Dropdown.Header>
-                    {types.map((type) => renderTypeTags(type))}
-                </Dropdown.Menu>
-            </Dropdown>
-        </form >
+        <Dropdown item simple icon="search">
+            <Dropdown.Menu>
+                <div className="ui input icon" >
+                    <Input type="text" name="term" onChange={e => filterByTerm(e.target.value)} placeholder="Search..." />
+                    <Icon aria-hidden="true" name="search" />
+                </div>
+                <Dropdown.Divider />
+                <Dropdown.Header>FILTER BY TYPE</Dropdown.Header>
+                {types.map((type) => renderTypeTags(type))}
+            </Dropdown.Menu>
+        </Dropdown>
     );
 };
 
 const mapStateToProps = state => {
     return { types: state.types };
 };
-
-FilterDropDown = reduxForm({ form: "searchForm" })(FilterDropDown);
 
 FilterDropDown = connect(mapStateToProps, { filterDolls })(FilterDropDown);
 
