af02f4512267769cbe6f266004b0f06f9ef33e64
Nahyeon Lee <naboee0627@gmail.com>
2021-04-01 23:21:32 +0300

    feat: Add react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Suspense, lazy } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 import MenuBar from "./Header/MenuBar";
 import Footer from "./Footer";
@@ -8,14 +9,27 @@ import Logo from "./Header/Logo";
 const App = () => {
   const DollList = lazy(() => import("./Doll/DollList"));
 
+  // Todo: Consider layout HOC
+  const PageComponent = Component => Object.assign(() => {
+    return (
+      <div>
+        <Logo />
+        <MenuBar />
+        <Suspense fallback={<Loader />}>
+          <Component />
+        </Suspense>
+        <Footer />
+      </div>
+    )
+  }, { displayName: "test" });
+
   return (
     <div>
-      <Logo />
-      <MenuBar />
-      <Suspense fallback={<Loader />}>
-        <DollList />
-      </Suspense>
-      <Footer />
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageComponent(DollList)} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
