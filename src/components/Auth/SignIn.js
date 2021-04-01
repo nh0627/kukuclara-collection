1fa72c6c408f7e64aadaf64d293451e560b14744
Nahyeon Lee <naboee0627@gmail.com>
2021-04-01 23:51:58 +0300

    fix: Update signin button

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { Menu, Button } from "semantic-ui-react";
-import { Link } from "react-router-dom";
 
 let SignIn = () => {
     return (
-        <Menu.Item as={Link} to="/signin">
+        <Menu.Item>
             <Button size="mini">Sign In</Button>
         </Menu.Item>
     )
